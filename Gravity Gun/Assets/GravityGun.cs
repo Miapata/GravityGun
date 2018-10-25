2a0235be76372586492e760cbd0f29c58d3d23e6
KNOWN-UNIVERSE\MIAPATA <miapata@uat.edu>
2018-10-25 09:27:45 -0700

    no message

---
+++ Gravity Gun/Assets/GravityGun.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGun : MonoBehaviour
+{
+    public GameObject gravityHolder;
+    public bool hasItem;
+
+    private GameObject toSlerp;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit raycast;
+        if (Input.GetMouseButton(0))
+        {
+            Physics.Raycast(gravityHolder.transform.position, transform.forward, out raycast);
+            if (raycast.collider != null)
+            {
+                if (raycast.collider.tag == "Object_Interactable")
+                {
+                    toSlerp = raycast.collider.gameObject;
+                }
+            }
+
+
+
+        }
+
+        if (toSlerp == null)
+            return;
+        LerpObject(toSlerp);
+    }
+
+    void LerpObject(GameObject hitGameObject)
+    {
+        if (hasItem == true)
+            return;
+        else
+        {
+            hitGameObject.transform.position = Vector3.Slerp(transform.position, gravityHolder.transform.position,  1f);
+        }
+    }
+}
---
+++ Gravity Gun/Assets/SmoothMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothMove : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    public GameObject playerTransform;
+    public Rigidbody rigid;
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        rigid.MovePosition(transform.position + (transform.forward * z ) + (transform.right * x ));
+        if (Input.GetKeyDown(KeyCode.Space))
+            rigid.AddForce(transform.up * jumpForce);
+    }
+
+}
+
