13cba6e1dd11fda2c99184866daac87bc5e67b94
KNOWN-UNIVERSE\MIAPATA <miapata@uat.edu>
2018-10-25 11:28:37 -0700

    no message

---
+++ Gravity Gun/Assets/WeaponSway.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSway : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+    float mousex;
+    float mousey;
+
+    Quaternion RotationSpeed;
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousex = Input.GetAxis("Mouse X");
+        mousey = Input.GetAxis("Mouse Y");
+
+        RotationSpeed = Quaternion.Euler(-mousey, mousex, 0);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, RotationSpeed, Speed * Time.deltaTime);
+    }
+}
+
